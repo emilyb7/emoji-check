7a84f1b53218578ed16a1984f776348177831559
emily <limeyb7@gmail.com>
2019-04-09 23:03:07 +0100

    ✨ validate commit msg

---
+++ script.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+const msg = fs.readFileSync(process.argv[2]).toString();
+
+const emojiRegex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+
+if (emojiRegex.test(msg)) {
+  console.log("\x1b[33m%s\x1b[0m", "✨ Great commit message!");
+} else {
+  console.log("🔥 Please add an emoji to your commit message and try again");
+  process.exit(1);
+}
