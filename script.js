d11ba133d0c146bd477dbf33aa76f8b46bb96fe0
emily <limeyb7@gmail.com>
2019-04-13 19:02:44 +0100

    💬 add better message copy

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const emojiRegex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d
 if (emojiRegex.test(msg)) {
   console.log("\x1b[33m%s\x1b[0m", "✨ Great commit message!");
 } else {
-  console.log("🔥 Please add an emoji to your commit message and try again");
+  console.log(
+    "\x1b[35m",
+    "🔥 🔥 🔥 Please add an emoji to your commit message and try again. \n For inspiration check https://gitmoji.carloscuesta.me/"
+  );
   process.exit(1);
 }
